f7fe97a65c137e810a12d8389a95f1d4248675c7
Jackson.Zhang <Jackson_Zhang07@yeah.net>
2017-06-18 17:42:43 +0800

    commit file throug Batch

---
+++ js/book.js
@@ -0,0 +1,7 @@
+function showBook(booklist){
+	var book_option_all = document.getElementById("all");
+	var book_option_html = document.getElementById("html");
+	if (book_option_html.checked){
+		alert("html is yes");
+	}
+}
